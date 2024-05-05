e182bbe8a26967ab869b748cc8412ae043d18a02
sukram-n <markus.wollgarten@gmx.net>
2024-05-05 23:13:28 +0200

    further improvements, better decoupling

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import time
 
 import streamlit as st
 
-import personal_accompanist
+from personal_accompanist import PersonalAccompanist
 
 
-def init_session() -> personal_accompanist.PersonalAccompanist:
+def init_session() -> PersonalAccompanist:
 
     st.set_page_config(layout="wide")
 
@@ -14,77 +14,55 @@ def init_session() -> personal_accompanist.PersonalAccompanist:
         if not os.path.exists(folder):
             os.mkdir(folder)
 
-    if 'pa' not in st.session_state:
-        # if keep our stuff in a separate class instance to avoid
-        # name space conflicts
-        st.session_state.pa = personal_accompanist.PersonalAccompanist()
+    st.session_state.p_a = PersonalAccompanist()
 
-    return st.session_state.pa
+    return st.session_state.p_a
 
 
-def clean_up():
+def clean_up(p_a) -> None:
 
-    pa = st.session_state.pa
-    # clean up
+    """
+    remove old files
+    """
 
-    for file in [f'.tmp/{pa.basename}.ly', f'.tmp/{pa.basename}-1.midi']:
-        if os.path.exists(file) and False:
-            os.remove(file)
-
-    for folder, age in [('./.tmp', 18), ('./.assets', 600)]:
+    # delete everything that is older than 600 seconds in the `./.tmp` folder
+    # and older the two hours in the `./.assets` folder
+    for folder, age in [('./.tmp', 60), ('./.assets', 7200)]:
         content = os.listdir(folder)
         # print(content)
         for entry in content:
             file = f'{folder}/{entry}'
             file_time = os.path.getmtime(file)
-            # Check against 2 hours, remove older stuff
             if (time.time() - file_time) > age:
                 os.remove(file)
 
 
 def main():
 
-    pa = init_session()
-    ss = st.session_state
+    rerun = False
 
-    if not pa.initial_run:
+    if "p_a" not in st.session_state:
+        # ok, then this is the initial run
+        # Prepare the app session with class instances and variables.
+        # We keep our stuff in an own class instance to avoid name space conflicts
+        # this instance is stored in `st.sessionstate.p_a`
 
-        if 'loop' in ss:
-            pa.loop = ss.loop
+        p_a = init_session()
+        rerun = True
 
-        if ss.exercise != pa.exercise:
-            pa.lilypond.is_valid &= False
-            pa.audio.is_valid &= False
-            pa.exercise = ss.exercise
+    else:
 
-        if ss.musickey != pa.key_text:
-            pa.lilypond.is_valid &= False
-            pa.audio.is_valid &= False
-            pa.key = ss.musickey.split('-')[0]
+        p_a = st.session_state.p_a
 
-        if ss.tempo != pa.tempo:
-            pa.lilypond.is_valid &= False
-            pa.audio.is_valid &= False
-            pa.tempo = ss.tempo
+        p_a.practice.update_variables()
+        p_a.lilypond.prepare()
+        p_a.audio.prepare()
 
-        if sorted(ss.speeds) != pa.speeds:
-            pa.lilypond.is_valid &= False
-            pa.audio.is_valid &= False
-            pa.speeds = sorted(ss.speeds)
+    p_a.gui.show_gui()
 
-        if ss.acc_instr != pa.acc_instr:
-            pa.audio.is_valid &= False
-            pa.acc_instr = ss.acc_instr
-
-    pa.lilypond.update()
-
-    pa.audio.update()
-
-    pa.widgets.update()
-
-    pa.initial_run = False
-
-    clean_up()
+    clean_up(st.session_state.p_a)
+    if rerun:
+        st.rerun()
 
 
 if __name__ == "__main__":
